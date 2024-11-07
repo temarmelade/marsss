dd96db74b2365b80c6fbce41544fd87f723146ae
Asanov Temirlan <temirlan.asanov@alatoo.edu.kg>
2024-11-07 15:51:05 +0600

    added the rules and the structure of the game

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,45 @@ public class Main {
             trueAnswer2 = 1 + (int) (Math.random() * 6);
             trueAnswer3 = 1 + (int) (Math.random() * 6);
         }
+        while (counter <= 5) {
+            int fTry = scanner.nextInt();
+            int sTry = scanner.nextInt();
+            int tTry = scanner.nextInt();
+            if ((fTry == trueAnswer1) || (fTry == trueAnswer2) || (fTry == trueAnswer3)) {
+                countF = 1;
+                if ((fTry == sTry) || (tTry == fTry)) {
+                    countF = -1;
+                }
+            }
+            if ((sTry == trueAnswer1) || (sTry == trueAnswer2) || (sTry == trueAnswer3)) {
+                countS = 1;
+                if ((tTry == sTry) || (sTry == fTry)) {
+                    countS = -1;
+                }
+            }
+            if ((tTry == trueAnswer1) || (tTry == trueAnswer2) || (tTry == trueAnswer3)) {
+                countT = 1;
+                if ((tTry == sTry) || (tTry == fTry)) {
+                    countT = -1;
+                }
+            }
+            check = (sTry == trueAnswer1) || (sTry == trueAnswer2) || (sTry == trueAnswer3) && (fTry == trueAnswer1) || (fTry == trueAnswer2) || (fTry == trueAnswer3) && (tTry == trueAnswer1) || (tTry == trueAnswer2) || (tTry == trueAnswer3);
+            int countSum = countF + countS + countT;
+            String det = (countSum == 1) ? "box" : "boxes";
+            if ((countSum == -3) || (countSum == -2)) {
+                System.out.println("You have found 1 box");
+            }
+            if (countSum == -1) {
+                System.out.println("You have found 2 box");
+            }
+            if ((countSum < 3) && (countSum > 0)) {
+                System.out.println("You have found " + countSum + " " + det);
+            }
+            if ((countF == 1) && (countS == 1) && (countT == 1) && (check)) {
+                System.out.println("Congratulations! You have won the game!");
+                break;
+            }
+            counter++;
+        }
     }
 }
